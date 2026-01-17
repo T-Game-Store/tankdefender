07db84f317ea1996e6d7506846707fe6cc1020e5
T-Aov247 <106479931+T-Game-Store@users.noreply.github.com>
2026-01-18 00:38:00 +0700

    Refactor Bullet class to include freezing and rotation

--- js/bullet.js
+++ js/bullet.js
@@ -1,17 +1,55 @@
 class Bullet {
-    constructor(x, y, vx, vy) {
-        this.x = x; this.y = y; this.vx = vx; this.vy = vy; this.size = 4;
-        this.isPiercing = false; this.hitEnemies = []; 
+    constructor(x, y, dx, dy, isFreezing = false, angle = 0) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        
+        this.angle = angle; 
+        
+        this.isFreezing = isFreezing === true; 
+        this.isPiercing = (currentSkinKey !== 'frozen' && isFreezing === false && keys['g']); 
+        
+        this.hitEnemies = [];
+        this._dead = false;
+
+        if (this.isFreezing) {
+            this.size = 35; 
+            this.img = new Image();
+            this.img.src = 'frozen_bullet.png';
+        } else {
+            this.size = this.isPiercing ? 10 : 5;
+        }
     }
+
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
     draw() {
         ctx.save();
-        ctx.fillStyle = this.isPiercing ? "#00ffff" : "#ff0000"; ctx.shadowBlur = 15; ctx.shadowColor = ctx.fillStyle;
-        ctx.beginPath(); ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); ctx.fill();
+        
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+
+        if (this.isFreezing) {
+            ctx.shadowBlur = 20;
+            ctx.shadowColor = "#00ffff";
+            if (this.img.complete) {
+                ctx.drawImage(this.img, -this.size, -this.size, this.size * 2, this.size * 2);
+            }
+        } else {
+            ctx.beginPath();
+            ctx.arc(0, 0, this.size, 0, Math.PI * 2);
+            ctx.fillStyle = this.isPiercing ? "blue" : "red";
+            ctx.fill();
+            ctx.closePath();
+        }
+        
         ctx.restore();
     }
-    update() { this.x += this.vx; this.y += this.vy; }
 }
-
 class BossBullet {
     constructor(x, y, vx, vy) {
         this.x = x; this.y = y; this.vx = vx; this.vy = vy;
