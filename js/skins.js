fbc3f1eb85964c8c4aaa5a66082edba1b191b35b
T-Aov247 <106479931+T-Game-Store@users.noreply.github.com>
2026-01-18 00:38:58 +0700

    Update skins.js

--- js/skins.js
+++ js/skins.js
@@ -14,6 +14,14 @@ const SKINS_CONFIG = {
         skillSound: 'choilamsaoduoc.mp3',
         speedBoost: 0,
         description: 'Âm thanh bộ pc mạnh mẽ'
+    },
+    'frozen': {
+        name: 'Frozen Tank',
+        img: 'frozen_tank.png', 
+        shootSound: 'shoot.mp3',
+        skillSound: 'skillshoot1.mp3', 
+        speedBoost: 0.5,
+        description: 'Lạnh giá và quyền năng: Đóng băng kẻ thù'
     }
 };
 
